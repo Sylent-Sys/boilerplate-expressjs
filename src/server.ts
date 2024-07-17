0e6113b9f4dc77b841bf0b1adbd93d0037a3e9d1
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-07-17 15:58:55 +0700

    chore: Add dotenv package for environment variables management

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import path from "path"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import cors from "cors"
+import "dotenv/config"
 const app = express()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
