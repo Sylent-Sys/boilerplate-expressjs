58eaffad0c9e44c21c5ba10421d9dd2957ef7536
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-07-28 00:35:55 +0700

    chore: split the location for convenient use

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,15 +1,14 @@
 import express from "express"
 import { router } from "express-file-routing"
 import path from "path"
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import cors from "cors"
 import "dotenv/config"
+import { __dirname } from "@/lib/location.js"
+import log from "@/middleware/log.js";
 const app = express()
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 app.use(cors())
 app.use(express.json())
+app.use(log())
 app.use("/", await router({
     directory: path.join(__dirname + "/../", "routes"),
 }))
