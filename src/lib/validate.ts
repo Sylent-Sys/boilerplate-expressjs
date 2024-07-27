45022433ed8455e349afab580b62b34939798524
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-07-28 00:36:48 +0700

    refactor: Update validate.ts to parse .env.example file for environment variables
    This code change in validate.ts updates the logic to parse the .env.example file and extract the environment variables. The file is read using fs.readFileSync and the content is split by newline to extract key-value pairs. The extracted variables are then stored in the envExampleVariables object. This change improves the validation process by ensuring that the .env.example file is correctly parsed and the variables are available for validation.
    
    This commit message follows the established convention of using a prefix to indicate the type of change, followed by a brief description of the change.

--- src/lib/validate.ts
+++ src/lib/validate.ts
@@ -1,14 +1,32 @@
 import { z } from 'zod';
+import { __dirname } from "@/lib/location.js"
+import fs from 'fs';
+import path from 'path';
+const envExampleFilePath = path.resolve(__dirname + "/../../", '.env.example');
+const envExampleFileContent = fs.readFileSync(envExampleFilePath, 'utf-8');
+const envExampleVariables: { [key: string]: string } = {};
+envExampleFileContent.split('\n').forEach(line => {
+    const [key, value] = line.split('=');
+    if (key && value) {
+        envExampleVariables[key.trim()] = value.trim().replaceAll(/"/g, '').replaceAll(/'/g, '').replaceAll(/`/g, '');
+    }
+});
 
 export const EnvSchema = z.object({
     DATABASE_URL: z.string(),
     JWT_SECRET: z.string(),
     APP_PORT: z.string(),
+    LOG: z.enum(["TRUE", "FALSE"]),
 });
 
 export const RoleSchema = z.enum(["USER", "ADMIN"]);
 
+const validateEnvExample = EnvSchema.safeParse(envExampleVariables);
 const validateEnv = EnvSchema.safeParse(process.env);
+if (!validateEnvExample.success) {
+    console.error(validateEnvExample.error.errors);
+    process.exit(1);
+}
 if (!validateEnv.success) {
     console.error(validateEnv.error.errors);
     process.exit(1);
